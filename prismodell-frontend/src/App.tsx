f00292a830e935adce6f622b34670ec6e9a4c508
Anuradha Bista <u60931@energycorp.com>
2025-04-24 13:21:36 +0200

    refactor

--- prismodell-frontend/src/App.tsx
+++ prismodell-frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Container, Text } from '@mantine/core';
+import { Box, Container, Grid, Text } from '@mantine/core';
 
 function App() {
   const [prisModel, setPrisModel] = useState('');
@@ -35,35 +35,40 @@ function App() {
 
   return (
     <Container fluid h={50} display='flex' style={{ marginTop: 50 }}>
-      <Box>
-        <input
-          style={{ height: 42, width: 250, marginRight: 50 }}
-          onChange={(event => onChangeConsumption(Number(event.target.value)))}
-          placeholder="Electricity consumption (KWH) per hour"
-        />
-        <select value={priceArea} onChange={(event) => onChangePriceAreaChange(event.target.value)} style={{ height: 50, width: 250 }}>
-          <option value="">Select price area</option>
-          {priceAreaOptions.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-        <select value={prisModel} onChange={(event) => onChangePriceModelChange(event.target.value)} style={{ height: 50, width: 250, marginLeft: 50 }}>
+      <Grid display='flex' justify='flex-start'>
+        <Grid.Col>
+
+          <input
+            style={{ height: 42, width: 250, marginRight: 50 }}
+            onChange={(event => onChangeConsumption(Number(event.target.value)))}
+            placeholder="Electricity consumption (KWH) per hour"
+          />
+        </Grid.Col>
+        <Grid.Col>
+          <select value={priceArea} onChange={(event) => onChangePriceAreaChange(event.target.value)} style={{ height: 50, width: 250, marginTop: 50 }}>
+            <option value="">Select price area</option>
+            {priceAreaOptions.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        </Grid.Col>
+        <Grid.Col><select value={prisModel} onChange={(event) => onChangePriceModelChange(event.target.value)} style={{ height: 50, width: 250, marginTop: 50 }}>
           <option value="">Select price model</option>
           {prisModelOptions.map((option) => (
             <option key={option.value} value={option.value}>
               {option.label}
             </option>
           ))}
-        </select>
-        <button onSubmit={handlePriceCalculation} style={{ height: 42, width: 250, marginRight: 50, marginLeft: 50 }}
-        >Calculate price</button>
-      </Box>
-      <Box>
-        <Text ta="right">Total price</Text>
-      </Box>
-    </Container>
+        </select></Grid.Col>
+        <Grid.Col> <button onSubmit={handlePriceCalculation} style={{ height: 50, width: 250, marginTop: 50, backgroundColor: 'green' }}
+        >Calculate price</button></Grid.Col>
+        <Grid.Col>
+          <Text ta="left">Total price : </Text>
+        </Grid.Col>
+      </Grid>
+    </Container >
   );
 }
 
